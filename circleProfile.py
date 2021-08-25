8e3109445e3d93289d5d47298263c759e1f14581
KaiwenJon <kaiwenjon23@gmail.com>
2021-08-25 15:16:49 +0800

    feat: predict multiple bounded profiles

--- circleProfile.py
+++ circleProfile.py
@@ -48,45 +48,58 @@ while(cap.isOpened()):
         for index, trajectory in enumerate(buffer):
             a, b, c = trajectory.fittingParams
             for circle in trajectory.circles:
-                cv2.circle(stats, (circle.x, circle.y), 1, (0, 0, 0), 3)
+                # cv2.circle(stats, (circle.x, circle.y), 1, (0, 0, 0), 3)
                 # cv2.putText(stats, str(trajectory.onGround), (circle.x + 5, circle.y + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
                 parabola_y = int(a * circle.x * circle.x + b * circle.x + c)
-                cv2.circle(stats, (circle.x, parabola_y), 1, (circle.rgb['B'], circle.rgb['G'], circle.rgb['R']), 3)
-    else:
+                # cv2.circle(stats, (circle.x, parabola_y), 1, (circle.rgb['B'], circle.rgb['G'], circle.rgb['R']), 3)
+
         # show predicted parabola.
         for trajectory in buffer:
             a, b, c = trajectory.fittingParams
             x = trajectory.getLatestCircle().x
             ground_height = 530
-
+            first_bounded_point = (0, 0)
+            first_bounded = False
             if trajectory.xDirection > 0:
+                hills_cnt = 0
                 while x < width:
                     parabola_y = int(a * x * x + b * x + c)
                     x = x + 1
                     if parabola_y > ground_height - trajectory.getLatestCircle().radius:
                         # onBound
+                        if not first_bounded:
+                            first_bounded_point = (x, parabola_y)
+                            first_bounded = True
                         D = b * b - 4 * a * (c - ground_height + trajectory.getLatestCircle().radius)
                         if D < 0:
                             continue
                         hill_distance = (math.sqrt(D))/a
-                        parabola_y = int(a * (x-hill_distance) * (x-hill_distance) + b * (x-hill_distance) + c)
+                        hills_cnt = math.floor((x - first_bounded_point[0]) / hill_distance) + 1
+                        # hills_cnt = 2
+                        parabola_y = int(a * (x-hill_distance * hills_cnt) * (x-hill_distance * hills_cnt) + b * (x-hill_distance * hills_cnt) + c)
                     if parabola_y > height:
                         continue
-                    cv2.circle(stats, (x, parabola_y), 1, (0, 0, 0))
+                    cv2.circle(stats, (x, parabola_y), 1, (trajectory.getLatestCircle().rgb['B'], trajectory.getLatestCircle().rgb['G'], trajectory.getLatestCircle().rgb['R']))
             elif trajectory.xDirection < 0:
                 while x > 0:
                     parabola_y = int(a * x * x + b * x + c)
                     x = x - 1
                     if parabola_y > ground_height - trajectory.getLatestCircle().radius:
                         # onBound
+                        if not first_bounded:
+                            first_bounded_point = (x, parabola_y)
+                            first_bounded = True
                         D = b * b - 4 * a * (c - ground_height + trajectory.getLatestCircle().radius)
                         if D < 0:
                             continue
                         hill_distance = (math.sqrt(D)) / a
-                        parabola_y = int(a * (x + hill_distance) * (x + hill_distance) + b * (x + hill_distance) + c)
+                        hills_cnt = math.floor((first_bounded_point[0] - x) / hill_distance) + 1
+                        # hills_cnt = 2
+                        parabola_y = int(a * (x + hill_distance * hills_cnt) * (x + hill_distance * hills_cnt) + b * (x + hill_distance * hills_cnt) + c)
                     if parabola_y > height:
                         continue
-                    cv2.circle(stats, (x, parabola_y), 1, (0, 0, 0))
+                    cv2.circle(stats, (x, parabola_y), 1, (trajectory.getLatestCircle().rgb['B'], trajectory.getLatestCircle().rgb['G'], trajectory.getLatestCircle().rgb['R']))
+
     cv2.imshow('statistics', stats)
 
     key = cv2.waitKey(1)
